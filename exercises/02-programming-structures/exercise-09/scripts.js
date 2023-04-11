7b2aaf10ed4764a884c23e5bd85fa4d2612fdd65
José Ferreira <joseguedes.dev@gmail.com>
2023-04-11 20:36:12 -0300

    Update exercise number 09 of the course

--- exercises/02-programming-structures/exercise-09/scripts.js
+++ exercises/02-programming-structures/exercise-09/scripts.js
@@ -1,7 +1,8 @@
 let base1 = 2;
 let base2 = 3;
 let base3 = 18;
+let exponent = 2;
 
-console.log("2 ^ 2 = " + Math.pow(2, 2));
-console.log("3 ^ 2 = " + Math.pow(3, 2));
-console.log("18 ^ 2 = " + Math.pow(18, 2));
+console.log(`${base1} ^ ${exponent} = ${Math.pow(base1, exponent)}`);
+console.log(`${base2} ^ ${exponent} = ${Math.pow(base2, exponent)}`);
+console.log(`${base3} ^ ${exponent} = ${Math.pow(base3, exponent)}`);
