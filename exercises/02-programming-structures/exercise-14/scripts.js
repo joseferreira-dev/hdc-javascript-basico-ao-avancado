fda96cf5f55f271c1b6319edd686733618b5aafc
José Ferreira <joseguedes.dev@gmail.com>
2023-04-11 20:57:48 -0300

    Add exercise number 14 of the course Adding the resolution of exercise number 14 of the course

---
+++ exercises/02-programming-structures/exercise-14/scripts.js
@@ -0,0 +1,9 @@
+for (let i = 0; i <= 50; i++) {
+	if (i == 0) {
+		console.log(`0 is a neutral number`);
+	} else if (i % 2 == 0) {
+		console.log(`${i} is an even number`);
+	} else {
+		console.log(`${i} is an odd number`);
+	}
+}
