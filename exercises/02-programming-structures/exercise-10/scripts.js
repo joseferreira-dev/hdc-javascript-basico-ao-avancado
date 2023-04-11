2a05330f430fd4bf5e7c0f0c9501ea27f64eb6f4
José Ferreira <joseguedes.dev@gmail.com>
2023-04-11 20:39:57 -0300

    Add exercise number 10 of the course
    Adding the resolution of exercise number 10 of the course

---
+++ exercises/02-programming-structures/exercise-10/scripts.js
@@ -0,0 +1,9 @@
+let velocity = 70;
+let speedLimit = 80;
+
+
+if (velocity <= speedLimit) {
+	console.log("You are below the speed limit");
+} else {
+	console.log("You are over the speed limit!");
+}
