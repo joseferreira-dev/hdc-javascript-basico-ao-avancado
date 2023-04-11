33e3ab4024ceb8cf31785d9b86a540d1b50dcd36
José Ferreira <joseguedes.dev@gmail.com>
2023-04-11 19:27:47 -0300

    Add exercise number 06 of the course
    Adding the resolution of exercise number 06 of the course

---
+++ exercises/02-programming-structures/exercise-06/scripts.js
@@ -0,0 +1,7 @@
+let dataTypeString = "This is a String";
+let dataTypeNumber = 20;
+let dataTypeBoolean = true;
+
+console.log(typeof dataTypeString);
+console.log(typeof dataTypeNumber);
+console.log(typeof dataTypeBoolean);
