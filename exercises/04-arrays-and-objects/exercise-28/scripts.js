637f51c6fa32c3e58b087581ce310c96c5fce1d5
José Ferreira <joseguedes.dev@gmail.com>
2023-04-12 20:29:27 -0300

    Add exercise number 28 of the course Adding the resolution of exercise number 28 of the course

---
+++ exercises/04-arrays-and-objects/exercise-28/scripts.js
@@ -0,0 +1,9 @@
+let bus = {
+	wheels: 8,
+	passengers: 40,
+	doors: 2
+}
+
+console.log(`Number of wheels = ${bus.wheels}`);
+console.log(`Number of passengers = ${bus.passengers}`);
+console.log(`Number of doors = ${bus.doors}`);
