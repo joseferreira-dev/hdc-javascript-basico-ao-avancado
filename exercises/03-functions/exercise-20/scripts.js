8e4cd06d03cfa7643d0f2891837f543ea33c0a6a
José Ferreira <joseguedes.dev@gmail.com>
2023-04-12 00:21:46 -0300

    Add exercise number 20 of the course Adding the resolution of exercise number 20 of the course

---
+++ exercises/03-functions/exercise-20/scripts.js
@@ -0,0 +1,10 @@
+function drivingAgeCheck(age) {
+	if (age >= 18) {
+		console.log("You can enroll in driving school");
+	} else {
+		console.log("You can not enroll in driving school");
+	}
+}
+
+drivingAgeCheck(17);
+drivingAgeCheck(21);
