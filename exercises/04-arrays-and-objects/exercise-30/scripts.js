496f092a13dba56de6985895818d92c0fd5a4b71
José Ferreira <joseguedes.dev@gmail.com>
2023-04-12 20:41:39 -0300

    Add exercise number 30 of the course Adding the resolution of exercise number 30 of the course

---
+++ exercises/04-arrays-and-objects/exercise-30/scripts.js
@@ -0,0 +1,9 @@
+let names = ['Jose', 'Mary', 'Marcus', 'Emily', 'Paul'];
+
+let myName = 'Jose';
+
+if (names.includes(myName)) {
+	console.log(`The name ${myName} is included in the array`);
+} else {
+	console.log(`The name ${myName} is not included in the array`);
+}
