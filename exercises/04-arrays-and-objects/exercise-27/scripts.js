9319bd6d84f6ee4f0bb6995edca457645e35fcee
José Ferreira <joseguedes.dev@gmail.com>
2023-04-12 20:25:43 -0300

    Add exercise number 27 of the course Adding the resolution of exercise number 27 of the course

---
+++ exercises/04-arrays-and-objects/exercise-27/scripts.js
@@ -0,0 +1,5 @@
+let countries = ['Brazil', 'USA'];
+let continents = ['America', 'Europe', 'Asia', 'Africa'];
+
+console.log(`${countries.length} elements`);
+console.log(`${continents.length} elements`);
