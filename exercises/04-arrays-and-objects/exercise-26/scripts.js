ec56a11c8c433a8fb9546debe223faddd20ec130
José Ferreira <joseguedes.dev@gmail.com>
2023-04-12 20:20:56 -0300

    Add exercise number 26 of the course
    Adding the resolution of exercise number 26 of the course

---
+++ exercises/04-arrays-and-objects/exercise-26/scripts.js
@@ -0,0 +1,5 @@
+let dragNames = ['Bianca DellaFancy', 'Duda Dellorusso', 'Dakota Monteiro', 'Lamona Divine', 'Kika Boom'];
+
+console.log(dragNames[0]);
+console.log(dragNames[2]);
+console.log(dragNames[3]);
