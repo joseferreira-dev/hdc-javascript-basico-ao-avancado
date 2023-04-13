7e4cdf237e62376e677f1a66e81502b1dbc0b1ce
José Ferreira <joseguedes.dev@gmail.com>
2023-04-12 21:08:55 -0300

    Add exercise number 35 of the course
    Adding the resolution of exercise number 35 of the course

---
+++ exercises/04-arrays-and-objects/exercise-35/scripts.js
@@ -0,0 +1,14 @@
+let calculator = {
+	sum: (a, b) => a + b,
+
+	subtraction: (a, b) => a - b,
+
+	division: (a, b) => a / b,
+
+	multiplication: (a, b) => a * b
+}
+
+console.log(calculator.sum(2, 5));
+console.log(calculator.subtraction(6, 3));
+console.log(calculator.division(8, 5));
+console.log(calculator.multiplication(1.25, 5));
