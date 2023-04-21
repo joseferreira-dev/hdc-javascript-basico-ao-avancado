e3190b9ee18728cd9e036013bbbb63bee75ae9f4
José Ferreira <joseguedes.dev@gmail.com>
2023-04-21 14:10:21 -0300

    Add exercise number 43 of the course
    Adding the resolution of exercise number 43 of the course

---
+++ exercises/06-regex/exercise-43/scripts.js
@@ -0,0 +1,7 @@
+let validBrand = /Brand: [NikeAdidasPumaAsics]/;
+
+console.log(validBrand.test("Brand: Nike"));
+console.log(validBrand.test("Brand: Adidas"));
+console.log(validBrand.test("Brand: Puma"));
+console.log(validBrand.test("Brand: Asics"));
+console.log(validBrand.test("Brand: Supreme"));
